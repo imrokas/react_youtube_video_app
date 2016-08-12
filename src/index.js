fd8be4ba24ff570a5f47550748313ef327f5e2b3
Rokas Zickevicius <rokas@imrokas.com>
2016-08-11 22:39:32 -0700

    test "hello world"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
+//import react
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+//Create a component that produces some HTML
+const App = function() => {
+	return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Take the components generated HTML and attach it to the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
